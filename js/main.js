61e04336d7eee71f67b41c4c99b4912c44712c04
yz5295 <yossish93@gmail.com>
2024-06-06 14:19:07 +0300

    message

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const login = document.querySelector("#login");
-const sginup = document.querySelector("#sginup");
-const buttonnewuser = document.getElementById("buttonnewuser");
-sginup.style.display = "none";
-buttonnewuser.addEventListener("click", function () {
-  login.style.display = "none";
-  sginup.style.display = "inline-block";
-});
